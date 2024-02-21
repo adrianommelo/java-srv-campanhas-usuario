69ec28f94efc6a19856e529fd9b6335dd5bd21df
Adriano Marinho <adrianomarinhomelo9@gmail.com>
2024-02-21 14:11:16 -0300

    build: incial commit

---
+++ src/main/java/br/com/campanha/usuario/javasrvcampanhasusuario/JavaSrvCampanhasUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.campanha.usuario.javasrvcampanhasusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSrvCampanhasUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSrvCampanhasUsuarioApplication.class, args);
+	}
+
+}
